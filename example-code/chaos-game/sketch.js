2ac26caeec7ee8923bd6d996bf2d8cf990e97c62
mdarfler <mdarfler@friendscentral.org>
2018-11-27 14:45:33 -0500

    previous != next

--- example-code/chaos-game/sketch.js
+++ example-code/chaos-game/sketch.js
@@ -1,37 +1,42 @@
 let current;
+let points;
+let previous;
 
 function setup() {
   createCanvas(windowHeight, windowHeight);
-	points = [];
-	const n = 5;
+  points = [];
+  const n = 5;
 
-	for (let i = 0; i < n; i++){
-		let angle = i * TWO_PI / n;
-		let v = p5.Vector.fromAngle(angle);
-		v. mult(width/2-10);
-		v.add(width/2, height/2);
-		points.push(v);
-	}
-	reset();
+  for (let i = 0; i < n; i++) {
+    let angle = i * TWO_PI / n;
+    let v = p5.Vector.fromAngle(angle);
+    v.mult(width / 2 - 10);
+    v.add(width / 2, height / 2);
+    points.push(v);
+  }
+  reset();
 }
 
-function reset(){
-	current = createVector(random(width), random(height));
-	background(0);
-	stroke(255);
-	strokeWeight(8);
-	for(let p of points) {
-		point(p.x, p.y);
-	}
+function reset() {
+  current = createVector(random(width), random(height));
+  background(0);
+  stroke(255);
+  strokeWeight(8);
+  for (let p of points) {
+    point(p.x, p.y);
+  }
 }
 
 function draw() {
-  for (let i = 0; i < 500; i++){
-		strokeWeight(1);
-		stroke(255,0,200,200);
-		let next = random(points);
-		current.x = lerp(current.x, next.x, 0.5);
-		current.y = lerp(current.y, next.y, 0.5);
-		point(current.x, current.y);
-	}
+  for (let i = 0; i < 500; i++) {
+    strokeWeight(1);
+    stroke(255, 0, 200, 200,200);
+    let next = random(points);
+    if (next != previous) {
+      current.x = lerp(current.x, next.x, 0.5);
+      current.y = lerp(current.y, next.y, 0.5);
+      point(current.x, current.y);
+    }
+		previous = next;
+  }
 }
